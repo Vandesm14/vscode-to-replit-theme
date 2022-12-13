7d29837fd9e49660edcf9edc1c849c09ec8afdcf
Shane Vandegrift <vandesm14@gmail.com>
2022-12-13 18:09:51 -0500

    stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,51 @@
+import React, { ChangeEvent } from 'react';
 import { createRoot } from 'react-dom/client';
 
+interface Token {
+  name?: string;
+  scope: string;
+  settings: {
+    foreground: string;
+  };
+}
+
+interface Theme {
+  name: string;
+  type: 'dark' | 'light';
+  semanticTokenColors: Record<string, { foreground: string }>;
+  tokenColors: Array<Token>;
+  colors: Record<string, string>;
+}
+
 function App() {
-  return <h1>Hello World!</h1>;
+  const [text, setText] = React.useState('');
+  const [json, setJson] = React.useState<Theme | null>();
+  const [colors, setColors] = React.useState<Array<string>>();
+
+  const onChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    try {
+      const asJson = JSON.parse(e.target.value);
+      setJson(asJson);
+    } catch (error) {
+      setText('Invalid JSON');
+      setJson(null);
+    }
+  };
+
+  return (
+    <>
+      <textarea onChange={onChange} value={text} />
+      <ul>
+        {colors
+          ? colors.map((color, index) => (
+              <li key={index}>
+                <span style={{ color: color }}>{color}</span>
+              </li>
+            ))
+          : null}
+      </ul>
+    </>
+  );
 }
 
 createRoot(document.getElementById('root')!).render(<App />);
